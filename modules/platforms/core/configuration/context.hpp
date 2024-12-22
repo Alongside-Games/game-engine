4a7ded907c2fa5f3c0b2339870301941c62d7b7e
iamdramaticleo <iamdramaticleo@gmail.com>
2024-12-22 16:46:28 +0200

    Fix inconsistent indentation in core files
    Adjusted spacing for variable declarations to ensure consistent code formatting. This improves code readability and maintains style uniformity across the project.

--- modules/platforms/core/configuration/context.hpp
+++ modules/platforms/core/configuration/context.hpp
@@ -4,8 +4,8 @@ namespace core::configuration
 {
     struct context
     {
-        static constexpr auto default_major_version { 4 };
-        static constexpr auto default_minor_version { 6 };
+        static constexpr auto   default_major_version { 4 };
+        static constexpr auto   default_minor_version { 6 };
 
         int32_t major_version { default_major_version };
         int32_t minor_version { default_minor_version };
