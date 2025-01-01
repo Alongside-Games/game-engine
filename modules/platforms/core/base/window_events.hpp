4aa51f64b0fc2a6092c2a349ea04cead1dc361a4
iamdramaticleo <iamdramaticleo@gmail.com>
2025-01-01 21:03:07 +0200

    Add `update` method declaration to the correct position
    Reordered the `update` method declaration in the `WindowEvents` class for better readability and logical grouping. This improves the structure and ensures consistency within the class definition.

--- modules/platforms/core/base/window_events.hpp
+++ modules/platforms/core/base/window_events.hpp
@@ -5,9 +5,9 @@ namespace core::base
     class WindowEvents
     {
     public:
+        virtual auto update() const -> void = 0;
+
         std::function<void(int32_t, int32_t)> on_size  { };
         std::function<void()>                 on_close { };
-
-        virtual auto update() const -> void = 0;
     };
 }
